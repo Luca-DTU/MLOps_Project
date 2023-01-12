98dd2dc35e2b81206f5b1e1faf1c76a48eaf67d5
Poul Gunnar Pii Svane <s153529@student.dtu.dk>
2023-01-12 16:53:55 +0100

    data import in clip data creates train, val test, datasets.

--- src/data/clip_data.py
+++ src/data/clip_data.py
@@ -2,6 +2,5 @@ import os
 import datasets
 
 COCO_DIR = os.path.join(os.getcwd(), "data", "zips")
-print(COCO_DIR)
 ds = datasets.load_dataset("ydshieh/coco_dataset_script", "2017", data_dir=COCO_DIR)
 print(ds)
