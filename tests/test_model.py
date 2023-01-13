1b2f9eb0b2b0f6da94f2c902b39917ef77869d91
BenjaminHenriksen <benjamin.henriksen@crconsulting.dk>
2023-01-13 16:45:31 +0100

    model_test.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,20 @@
+from src.models.model import Transformer
 
-model = ()
-devices = ["cpu", "GPU"]
-#assert model.device == device
+model = Transformer()
+devices = ["cpu", "CUDA"]
+
+def test_device():
+    assert model.device in devices, "device is not recognized"
+
+num_labels = 5
+
+def test_num_labels():
+    assert model.num_labels == num_labels, "num_labels is not recognized"
+
+
+def test_size():
+    assert model.__sizeof__() == 32, "size of model misshapen"
+    assert model.parameters().__sizeof__() == 96, "wrong number of parameters"
+
+def test_input_embeddings():
+    assert model.get_input_embeddings().embedding_dim == 768, "input is misshapen"
