37646fd477e528b7b3050f10c59c6411c8b2b348
lucab <lucabertolani98@gmail.com>
2023-01-13 15:06:52 +0100

    pep8 on make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,7 +17,15 @@ import os
 
 
 class yelp_dataset(Dataset):
-    def __init__(cfg,self, train: bool, in_folder: str = "", out_folder: str = "",seed : int = 0 ,size : int = 0) -> None:
+    def __init__(
+        cfg,
+        self,
+        train: bool,
+        in_folder: str = "",
+        out_folder: str = "",
+        seed: int = 0,
+        size: int = 0,
+    ) -> None:
         super().__init__()
         self.seed = seed
         self.size = size
@@ -36,14 +44,19 @@ class yelp_dataset(Dataset):
         self.download_data()
 
         ########## Preprocessing ##########
-        tokenizer = AutoTokenizer.from_pretrained("bert-base-cased") #Load tokenizer
-        #Run tokenizer on dataset
+        tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")  # Load tokenizer
+        # Run tokenizer on dataset
         def tokenize_function(examples):
             return tokenizer(examples["text"], padding="max_length", truncation=True)
+
         self.tokenized_datasets = self.dataset.map(tokenize_function, batched=True)
-        #Get a subset of dataset
+        # Get a subset of dataset
         split = "train" if self.train else "test"
-        self.data = self.tokenized_datasets[split].shuffle(seed=self.seed).select(range(self.size))
+        self.data = (
+            self.tokenized_datasets[split]
+            .shuffle(seed=self.seed)
+            .select(range(self.size))
+        )
         ##################################
 
         if self.out_folder:
@@ -51,27 +64,26 @@ class yelp_dataset(Dataset):
 
     def save_preprocessed(self) -> None:
         split = "/train" if self.train else "/test"
-        self.data.save_to_disk(self.out_folder+split) #Save to folder
+        self.data.save_to_disk(self.out_folder + split)  # Save to folder
 
     def load_preprocessed(self) -> None:
         split = "/train" if self.train else "/test"
         try:
-            self.data = datasets.load_from_disk(self.out_folder+split)
+            self.data = datasets.load_from_disk(self.out_folder + split)
         except:
             raise ValueError("No preprocessed files found")
 
     def download_data(self) -> None:
         try:
-            self.dataset = datasets.load_from_disk(self.in_folder) #Load already downloaded data
+            self.dataset = datasets.load_from_disk(
+                self.in_folder
+            )  # Load already downloaded data
         except:
-            self.dataset = load_dataset("yelp_review_full") #Download data
-            self.dataset.save_to_disk(self.in_folder) #Save to folder
+            self.dataset = load_dataset("yelp_review_full")  # Download data
+            self.dataset.save_to_disk(self.in_folder)  # Save to folder
 
 
-
-
-
-@hydra.main(config_path = os.path.join(os.getcwd(),'conf'), config_name='config.yaml')
+@hydra.main(config_path=os.path.join(os.getcwd(), "conf"), config_name="config.yaml")
 def main(cfg) -> None:
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
@@ -84,9 +96,21 @@ def main(cfg) -> None:
     input_filepath = cfg.data.input_filepath
     output_filepath = cfg.data.output_filepath
 
-    train = yelp_dataset(train=True, in_folder=input_filepath, out_folder=output_filepath,seed = seed,size=size)
+    train = yelp_dataset(
+        train=True,
+        in_folder=input_filepath,
+        out_folder=output_filepath,
+        seed=seed,
+        size=size,
+    )
 
-    test = yelp_dataset(train=False, in_folder=input_filepath, out_folder=output_filepath,seed=seed,size=size)
+    test = yelp_dataset(
+        train=False,
+        in_folder=input_filepath,
+        out_folder=output_filepath,
+        seed=seed,
+        size=size,
+    )
 
 
 if __name__ == "__main__":
