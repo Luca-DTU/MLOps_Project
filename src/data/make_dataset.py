a29c626dbdd2b2a72ed046d39a17f67ca4e63c73
Andreas With Aspe <s174197@student.dtu.dk>
2023-01-13 15:03:21 +0100

    A lot of strange files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -12,13 +12,15 @@ from torch.utils.data import Dataset
 import datasets
 from datasets import load_dataset
 from transformers import AutoTokenizer
-
+import hydra
+import os
 
 
 class yelp_dataset(Dataset):
-    def __init__(self, train: bool, in_folder: str = "", out_folder: str = "") -> None:
+    def __init__(cfg,self, train: bool, in_folder: str = "", out_folder: str = "",seed : int = 0 ,size : int = 0) -> None:
         super().__init__()
-
+        self.seed = seed
+        self.size = size
         self.train = train
         self.in_folder = in_folder
         self.out_folder = out_folder
@@ -41,7 +43,7 @@ class yelp_dataset(Dataset):
         self.tokenized_datasets = self.dataset.map(tokenize_function, batched=True)
         #Get a subset of dataset
         split = "train" if self.train else "test"
-        self.data = self.tokenized_datasets[split].shuffle(seed=42).select(range(1000))
+        self.data = self.tokenized_datasets[split].shuffle(seed=self.seed).select(range(self.size))
         ##################################
 
         if self.out_folder:
@@ -67,19 +69,24 @@ class yelp_dataset(Dataset):
 
 
 
-@click.command()
-@click.argument("input_filepath", type=click.Path(exists=False))
-@click.argument("output_filepath", type=click.Path())
-def main(input_filepath: str, output_filepath: str) -> None:
+
+
+@hydra.main(config_path = os.path.join(os.getcwd(),'conf'), config_name='config.yaml')
+def main(cfg) -> None:
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
 
-    train = yelp_dataset(train=True, in_folder=input_filepath, out_folder=output_filepath)
+    seed = cfg.data.seed
+    size = cfg.data.size
+    input_filepath = cfg.data.input_filepath
+    output_filepath = cfg.data.output_filepath
 
-    test = yelp_dataset(train=False, in_folder=input_filepath, out_folder=output_filepath)
+    train = yelp_dataset(train=True, in_folder=input_filepath, out_folder=output_filepath,seed = seed,size=size)
+
+    test = yelp_dataset(train=False, in_folder=input_filepath, out_folder=output_filepath,seed=seed,size=size)
 
 
 if __name__ == "__main__":
