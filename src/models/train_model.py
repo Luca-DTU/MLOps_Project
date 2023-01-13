0325d37c8cb4bd845ef58a62882e31aba6be013c
lucab <lucabertolani98@gmail.com>
2023-01-13 11:34:48 +0100

    data update

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+import datasets
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 from transformers import TrainingArguments
@@ -7,8 +7,7 @@ import evaluate
 from transformers import TrainingArguments, Trainer
 
 #Load data
-dataset = load_dataset("yelp_review_full")
-dataset["train"][100]
+dataset = datasets.load_from_disk('data/raw')
 
 #Load tokenizer
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
