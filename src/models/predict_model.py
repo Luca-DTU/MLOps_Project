56716d2c857934b3e3e596af453240c085e4d3db
lucab <lucabertolani98@gmail.com>
2023-01-16 11:03:59 +0100

    pep 8 compliance

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,7 +1,8 @@
 from src.models.model import transformer
 from transformers import TrainingArguments, Trainer
 
+
 def predict_model(path=None):
     model = transformer(path)
-    test_dataset =
+    test_dataset = 0
     trainer = Trainer(model)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -11,35 +11,50 @@ import os
 
 from src.data.make_dataset import yelp_dataset
 from src.models.model import transformer
-cfg = omegaconf.OmegaConf.load('conf/config.yaml')
 
-#Define metric function
+cfg = omegaconf.OmegaConf.load("conf/config.yaml")
+
+# Define metric function
 def compute_metrics(eval_pred):
-    #Define metric
+    # Define metric
     metric = evaluate.load("accuracy")
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
-#Define training arguments
-@hydra.main(config_path = os.path.join(os.getcwd(),'conf'), config_name='config.yaml')
+
+# Define training arguments
+@hydra.main(config_path=os.path.join(os.getcwd(), "conf"), config_name="config.yaml")
 def load_training_cfg(cfg):
     info = cfg.model
-    training_args = TrainingArguments(None,**info)
+    training_args = TrainingArguments(None, **info)
     return training_args
+
 
 def main():
     seed = cfg.data.seed
     size = cfg.data.size
-    train_set = yelp_dataset(train=True, in_folder=cfg.data.input_filepath, out_folder=cfg.data.output_filepath,seed = seed,size = size).data
-    test_set = yelp_dataset(train=False, in_folder=cfg.data.input_filepath, out_folder=cfg.data.output_filepath,seed = seed,size = size).data
+    train_set = yelp_dataset(
+        train=True,
+        in_folder=cfg.data.input_filepath,
+        out_folder=cfg.data.output_filepath,
+        seed=seed,
+        size=size,
+    ).data
+    test_set = yelp_dataset(
+        train=False,
+        in_folder=cfg.data.input_filepath,
+        out_folder=cfg.data.output_filepath,
+        seed=seed,
+        size=size,
+    ).data
 
     # Download the pretrained model
     model = transformer("models/pre_trained")
     # load training configuration from cfg file
     training_args = load_training_cfg()
 
-    #Define trainer
+    # Define trainer
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -48,9 +63,10 @@ def main():
         compute_metrics=compute_metrics,
     )
 
-    #Train!
+    # Train!
     trainer.train()
     trainer.save_model("models/experiments")
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
