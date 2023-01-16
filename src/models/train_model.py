5dfe97b363abdf352ee82ea79e69197f4d0b4233
lucab <lucabertolani98@gmail.com>
2023-01-16 10:48:00 +0100

    refactoring of train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -10,20 +10,13 @@ import hydra
 import os
 
 from src.data.make_dataset import yelp_dataset
-from src.models.model import Transformer
+from src.models.model import transformer
 cfg = omegaconf.OmegaConf.load('conf/config.yaml')
-
-train_set = yelp_dataset(train=True, in_folder="data/raw", out_folder="data/processed").data
-test_set = yelp_dataset(train=False, in_folder="data/raw", out_folder="data/processed").data
-
-#Download the pretrained model
-model = Transformer()
-
-#Define metric
-metric = evaluate.load("accuracy")
 
 #Define metric function
 def compute_metrics(eval_pred):
+    #Define metric
+    metric = evaluate.load("accuracy")
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
@@ -35,17 +28,29 @@ def load_training_cfg(cfg):
     training_args = TrainingArguments(None,**info)
     return training_args
 
-training_args = load_training_cfg()
+def main():
+    seed = cfg.data.seed
+    size = cfg.data.size
+    train_set = yelp_dataset(train=True, in_folder=cfg.data.input_filepath, out_folder=cfg.data.output_filepath,seed = seed,size = size).data
+    test_set = yelp_dataset(train=False, in_folder=cfg.data.input_filepath, out_folder=cfg.data.output_filepath,seed = seed,size = size).data
 
-#Define trainer
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_set,
-    eval_dataset=test_set,
-    compute_metrics=compute_metrics,
-)
+    # Download the pretrained model
+    model = transformer("models/pre_trained")
+    # load training configuration from cfg file
+    training_args = load_training_cfg()
 
-#Train!
-trainer.train()
-trainer.save_model("models/experiments")
+    #Define trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_set,
+        eval_dataset=test_set,
+        compute_metrics=compute_metrics,
+    )
+
+    #Train!
+    trainer.train()
+    trainer.save_model("models/experiments")
+
+if __name__=='__main__':
+    main()
