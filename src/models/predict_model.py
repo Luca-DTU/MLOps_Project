f598497cf6344adf128e1752a6febf9c6faef0f2
Poul Gunnar Pii Svane <s153529@student.dtu.dk>
2023-01-17 10:46:21 +0100

    prelim. pred. file

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,14 +5,16 @@ from src.data.make_dataset import yelp_dataset
 from src.models.train_model import compute_metrics
 import hydra
 import numpy as np
+from train_model import compute_metrics
 
 
 @hydra.main(config_path=os.path.join(os.getcwd(), "conf"), config_name="config.yaml")
-def predict_model(cfg, path=None):
+def predict_model(cfg):
     seed = cfg.data.seed
-    size = cfg.data.size
+    size = cfg.predict.size
     input_filepath = cfg.data.input_filepath
     output_filepath = cfg.data.output_filepath
+    path = cfg.predict.model_path
 
     test_args = TrainingArguments(
         output_dir="test_trainer",
@@ -35,7 +37,7 @@ def predict_model(cfg, path=None):
                       args=test_args,
                       compute_metrics=compute_metrics)
 
-    predictions, labels, metrics = trainer.predict(data, metric_key_prefix="predict")
+    predictions, labels, metrics = trainer.predict(data)
 
     metrics["predict_samples"] = len(data)
 
@@ -43,13 +45,13 @@ def predict_model(cfg, path=None):
     trainer.save_metrics("predict", metrics)
 
     predictions = np.argmax(predictions, axis=1)
-    pred_output_dir = os.path.join(os.cwd(),"predictions")
-    output_predict_file = os.path.join(pred_output_dir, "predictions.txt")
+    output_predict_file = os.path.join(os.getcwd(), cfg.predict.save_path, "predictions.txt")
     if trainer.is_world_process_zero():
         with open(output_predict_file, "w") as writer:
             writer.write("index\tprediction\n")
             for index, item in enumerate(predictions):
+                print(f"{index}\t{item}\t{labels[index]}")
                 writer.write(f"{index}\t{item}\n")
 
 
-predict_model(path="models/pre_trained")
+predict_model()
